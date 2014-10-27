64dc2d10e4dbaab0c5fd670d7ea373ec0e139222
Scott Stafford <scott.stafford@gmail.com>
2014-10-27 11:04:32 -0400

    Added fixes to call showcompare argument.
    tgit showcompare /revision1:HEAD /revision2:<other_branch> # diff two branches.

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ This very simple batch file allows convenient commandline access to some of the 
     tgit commit
     tgit sync
     tgit diff [path]    # invoke the diff tool registered in TortoiseGit.
+    tgit showcompare /revision1:HEAD /revision2:<other_branch> # diff two branches. 
 
-Based on Ayende's work at http://www.ayende.com/blog/4749/executing-tortoisegit-from-the-command-line.
+Based on Ayende's work at http://www.ayende.com/blog/4749/executing-tortoisegit-from-the-command-line
+and documentation at http://tortoisegit.org/docs/tortoisegit/tgit-automation.html.
 
 And, like him, I am posting this mostly because I want to be able to look it up afterward.
--- tgit.bat
+++ tgit.bat
@@ -3,11 +3,31 @@
 
 @REM Default the path argument to ., otherwise the second argument is the path.
 set TGITPATH=.
-if "%2" == "" goto execute
 
+if "%2" == "" goto execute
+call :StartsWith "%2" "/" &&goto execute ||goto setpath
+
+:setpath
 set TGITPATH=%2
+shift /2
 
 :execute
 @REM You may need to adjust the .exe path below, either with an absolute path
 @REM or by changing it to TortoiseProc.exe, used in older versions of TortoiseGit.
-@start TortoiseGitProc.exe /command:"%1" /path:"%TGITPATH%" %2 %3
+@start TortoiseGitProc.exe /command:"%1" /path:"%TGITPATH%" %2 %3 %4
+
+goto end
+
+:StartsWith text string -- Tests if a text starts with a given string
+::                      -- [IN] text   - text to be searched
+::                      -- [IN] string - string to be tested for
+:$created 20080320 :$changed 20080320 :$categories StringOperation,Condition
+:$source http://www.dostips.com
+SETLOCAL
+set "txt=%~1"
+set "str=%~2"
+if defined str call set "s=%str%%%txt:*%str%=%%"
+if /i "%txt%" NEQ "%s%" set /a $err = 1
+EXIT /b %$err%
+
+:end
