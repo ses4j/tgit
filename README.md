1926e51216997c2ed8add064e0bb2bf0e5747a4d
Scott Stafford <scott.stafford@gmail.com>
2014-10-27 11:34:16 -0400

    Built a more 'git'-like interface for simple branch comparisons.

--- README.md
+++ README.md
@@ -6,10 +6,11 @@ Windows batch file to allow invoking TortoiseGit naturally via commandline.
 This very simple batch file allows convenient commandline access to some of the lovely TortoiseGit GUI tools, such as:
 
     tgit log [path]
+    tgit log ..branch 		# show commits between HEAD and `branch`
     tgit commit
     tgit sync
-    tgit diff [path]    # invoke the diff tool registered in TortoiseGit.
-    tgit showcompare /revision1:HEAD /revision2:<other_branch> # diff two branches. 
+    tgit diff [path]    	# invoke the diff tool registered in TortoiseGit.
+    tgit diff ..branch 		# diff HEAD against `branch`
 
 Based on Ayende's work at http://www.ayende.com/blog/4749/executing-tortoisegit-from-the-command-line
 and documentation at http://tortoisegit.org/docs/tortoisegit/tgit-automation.html.
--- tgit.bat
+++ tgit.bat
@@ -3,18 +3,34 @@
 
 @REM Default the path argument to ., otherwise the second argument is the path.
 set TGITPATH=.
-
-if "%2" == "" goto execute
-call :StartsWith "%2" "/" &&goto execute ||goto setpath
+set BASEREV=
+set MAINCOMMAND=%1
+set FIRSTARG=%2
+@REM If no arguments, execute with default path.
+if "%FIRSTARG%" == "" goto execute
+@REM If arguments start with /, it's not a path.
+call :StartsWith "%FIRSTARG%" "/" &&goto execute
+@REM If argument starts with .., it's a relative branch,
+call :StartsWith "%FIRSTARG%" ".." &&goto parsebranch
 
 :setpath
 set TGITPATH=%2
 shift /2
+goto execute
+
+:parsebranch
+if "%MAINCOMMAND%"=="log" set BASEREV=/startrev:HEAD /endrev:%FIRSTARG:~2%
+if "%MAINCOMMAND%"=="diff" set MAINCOMMAND=showcompare
+if "%MAINCOMMAND%"=="showcompare" set BASEREV=/revision1:HEAD /revision2:%FIRSTARG:~2%
+shift /2
+
+goto execute
 
 :execute
 @REM You may need to adjust the .exe path below, either with an absolute path
 @REM or by changing it to TortoiseProc.exe, used in older versions of TortoiseGit.
-@start TortoiseGitProc.exe /command:"%1" /path:"%TGITPATH%" %2 %3 %4
+echo Executing: TortoiseGitProc.exe /command:"%MAINCOMMAND%" /path:"%TGITPATH%" %BASEREV% %2 %3 %4
+@start TortoiseGitProc.exe /command:"%MAINCOMMAND%" /path:"%TGITPATH%" %BASEREV% %2 %3 %4
 
 goto end
 
